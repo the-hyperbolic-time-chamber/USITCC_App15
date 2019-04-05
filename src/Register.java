1c55fdda2a98f66931a0e33e8a3d76af9830dd68
zackp <zackpatton@gmail.com>
2019-04-05 14:15:15 -0500

    Update Register.java

--- src/Register.java
+++ src/Register.java
@@ -1,9 +1,27 @@
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
 
 public class Register extends Composite {
 	private Text text;
+	
+	public static void main (String[] args)
+	{
+	    Display display = new Display();
+	    Shell shell = new Shell(display);
+	    shell.setLayout(new GridLayout(1, false));
+	    Register reg = new Register(shell, SWT.NONE);
+	    shell.pack();
+	    shell.open();
+	    while (!shell.isDisposed()) {
+	      if (!display.readAndDispatch())
+	        display.sleep();
+	    }
+	    display.dispose();
+	}
 
 	/**
 	 * Create the composite.
